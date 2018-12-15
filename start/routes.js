ae41d6b4580d5947bff28414e5c545a0c7fede87
sunliquan <sunliquan@sunliquandeMacBook-Pro.local>
2018-12-16 00:32:24 +0800

    patch put delete

--- start/routes.js
+++ start/routes.js
@@ -22,9 +22,23 @@ Route.get('/posts', () => 'List of get posts')
 
 Route.post('/posts', () => 'post moeths has created')
 
-
 // 路由参数
 Route.get('/postsWidthParams/:id', ({ params }) => {
     return `you're watching post ${params.id}.`
 
+})
+
+// patch/put 修改
+Route.patch('/postsWidthParams/:id', ({ params }) => {
+    return `Post ${params.id}  as been update.`
+})
+
+// patch/put 修改
+Route.put('/postsWidthParams/:id', ({ params }) => {
+    return `Post ${params.id} has been update.`
+})
+
+// delete 删除
+Route.delete('/postsWidthParams/:id', ({ params }) => {
+    return `Post ${params.id} has been delete.`
 })
